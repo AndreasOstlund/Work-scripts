13774f5b09263d818d64236f2877125aea0078ac
AndreasOstlund <AndreasOstlund@users.noreply.github.com>
2017-09-19 10:20:44 +0200

    .

---
+++ reg_export_for_diff.ps1
@@ -0,0 +1,10 @@
+﻿$prefix = Read-Host -Prompt "prefix"
+$exportdir = Read-Host -Prompt "export to"
+
+$ts = (get-date).ToSTring("yyyyMMdd_HHmmss")
+$ts
+
+reg export HKLM\Software $(join-path -Path $exportdir -ChildPath "HKLM_${prefix}_${ts}")
+reg export HKCU\Software $(join-path -path $exportdir -childpath "HKCU_${prefix}_${ts}")
+
+
