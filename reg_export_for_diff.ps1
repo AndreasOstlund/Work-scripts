1d6e9595aaa43d43267a6a56318d77737915922f
AndreasOstlund <AndreasOstlund@users.noreply.github.com>
2017-09-26 10:00:56 +0200

    options as params

--- reg_export_for_diff.ps1
+++ reg_export_for_diff.ps1
@@ -1,10 +1,16 @@
-﻿$prefix = Read-Host -Prompt "prefix"
-$exportdir = Read-Host -Prompt "export to"
+﻿[cmdletBinding()]
+Param(
+    [string]$prefix
+    ,[string]$exportdir
+)
+
+if(-not $prefix) { $prefix = Read-Host -Prompt "prefix" }
+if(-not $exportdir) { $exportdir = Read-Host -Prompt "export to" }
 
 $ts = (get-date).ToSTring("yyyyMMdd_HHmmss")
 $ts
 
-reg export HKLM\Software $(join-path -Path $exportdir -ChildPath "HKLM_${prefix}_${ts}")
-reg export HKCU\Software $(join-path -path $exportdir -childpath "HKCU_${prefix}_${ts}")
+reg export HKLM\Software $(join-path -Path $exportdir -ChildPath "HKLM_${prefix}_${ts}.reg")
+reg export HKCU\Software $(join-path -path $exportdir -childpath "HKCU_${prefix}_${ts}.reg")
 
 
