2d2d49af2a58e1b3b259139e167e98610a316ecb
AndreasOstlund <AndreasOstlund@noreply.users.github.com>
2018-06-24 01:15:00 +0200

    ps script to find and close file handles held by atom

---
+++ find_atom_handles.ps1
@@ -0,0 +1,22 @@
+﻿
+# find atom processes that has no child processe
+$AtomProcs = Get-CimInstance -ClassName Win32_Process 
+$TopLevelProcs = $AtomProcs | ? { $_.Name -eq "atom.exe" } | ForEach-Object {
+    $proc = $_
+    if(-not $($AtomProcs | ? { $_.ProcessId -eq $proc.ParentProcessId }) ) {
+        $proc
+    }
+}
+
+$TopLevelProcs
+
+$HandleBin = ".\handle64.exe"
+$PathPattern = "directory\\directory2\\"
+& $HandleBin -a -p atom | Select-String -Pattern $PathPattern | `
+    ForEach-Object { 
+        $AA=[regex]::split($_.Line.ToString(),'(.*): File.*')
+        $Handle=$AA[1].replace(' ','')
+        & $HandleBin -c $Handle -y -p 11860
+    }
+
+
